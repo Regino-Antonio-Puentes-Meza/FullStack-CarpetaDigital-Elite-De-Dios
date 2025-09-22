24ae8b7ad29b4638a2d47be2ac7f9ec317734dc4
Regino Antonio Puentes Meza <regino.puentes@globalmvm.com>
2025-09-22 01:47:20 -0500

    Mejoras y actualización

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 
-  // Cargar museum.html
+  // Cargar pag-menu-principal.html al hacer clic en el botón zarpar
   document.getElementById("enter-museum-btn").addEventListener("click", () => {
 
-    window.location.href = "pages/museum.html";
+    window.location.href = "pages/pag-menu-principal.html";
   });
